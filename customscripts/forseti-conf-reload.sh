5e35bfb09579a1520c341e310335e06555575dd1
Jean <jean.hamman@sharpspring.com>
2019-06-27 10:15:13 -0400

    custom scripts

---
+++ customscripts/forseti-conf-reload.sh
@@ -0,0 +1,2 @@
+gsutil cp gs://forseti-server-cd5fa93/configs/forseti_conf_server.yaml /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml
+forseti server configuration reload gs://forseti-server-cd5fa93/configs/forseti_conf_server.yaml
