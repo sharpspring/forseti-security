0456033824ddd33d19d7bba81f64b5f3879ddcfa
Jean <jean.hamman@sharpspring.com>
2020-04-17 18:51:13 -0400

    1

--- customscripts/forseti-conf-reload.sh
+++
@@ -1,2 +0,0 @@
-gsutil cp gs://forseti-server-cd5fa93/configs/forseti_conf_server.yaml /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml
-forseti server configuration reload gs://forseti-server-cd5fa93/configs/forseti_conf_server.yaml
--- customscripts/run-forseti.sh
+++
@@ -1,5 +0,0 @@
-MODEL_ID=$(/bin/date -u +%Y%m%dT%H%M%S)
-forseti inventory create --import_as ${MODEL_ID}
-forseti model use ${MODEL_ID}
-forseti scanner run
-forseti notifier run
